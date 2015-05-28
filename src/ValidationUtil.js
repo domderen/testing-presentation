23fea1a2ec755d1619b00092237d1c8ea9aa9052
Dominik Deren <dominik.deren@live.com>
2015-05-28 13:51:14 +0100

    Write your test

--- src/ValidationUtil.js
+++ src/ValidationUtil.js
@@ -1,3 +1,3 @@
-export function isString(obj) { 
+export function isString(obj) {
   return typeof obj === 'string';
 };
---
+++ test/unit/ValidationUtil-test.js
@@ -0,0 +1,12 @@
+import {expect} from 'chai';
+
+import * as ValidationUtil from './../../src/ValidationUtil';
+
+describe('ValidationUtil', () => {
+	describe('isString', () => {
+    it('should return true, when a string value is passed in', () => {
+      const result = ValidationUtil.isString('some string');
+      expect(result).to.be.true;
+    });
+  });
+});
