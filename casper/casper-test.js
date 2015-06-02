a48830facd99df483f812cd5148893736d70a4a1
Dominik Deren <dominik.deren@live.com>
2015-06-02 21:38:03 +0100

    E2E Tests - Casper.js

---
+++ casper/casper-test.js
@@ -0,0 +1,27 @@
+casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
+  casper.start("http://www.google.fr/", function() {
+    test.assertTitle("Google", "google homepage title is the one expected");
+    test.assertExists('form[action="/search"]', "main form is found");
+    this.fill('form[action="/search"]', {
+        q: "casperjs"
+    }, true);
+  });
+
+  casper.then(function() {
+    test.assertTitle("casperjs - Google Search", "google title is ok");
+    test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
+    test.assertEval(function() {
+        return __utils__.findAll("h3.r").length >= 10;
+    }, "google search for \"casperjs\" retrieves 10 or more results");
+  });
+
+  casper.run(function() {
+    test.done();
+
+    // work around "Unsafe JavaScript attempt to access frame with URL about:blank" error
+    // http://stackoverflow.com/questions/26608391/using-phantomjs-to-embed-all-images-of-a-webpage-produces-warnings-but-works/26688062#26688062
+    setTimeout(function(){
+      phantom.exit();
+    }, 0);
+  });
+});
